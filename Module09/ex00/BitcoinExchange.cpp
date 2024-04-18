e1441e1a674a54772b9f0ca31f91a9f4b4ed1bbe
Juan Antonio <juanantoniomartinezmorales@Juans-Laptop.local>
2024-04-18 23:17:07 +0200

    Module09 ex00 finished

--- Module09/ex00/BitcoinExchange.cpp
+++ Module09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: juan-anm < juan-anm@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 23:31:36 by juan-anm          #+#    #+#             */
-/*   Updated: 2024/04/18 19:14:47 by juan-anm         ###   ########.fr       */
+/*   Updated: 2024/04/18 23:14:59 by juan-anm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ bitCoin::bitCoin(const std::string& csv_file)
 	_csv_db.open(csv_file, std::ifstream::in);
 	if (!_csv_db.is_open())
 		throw std::invalid_argument("Invalid file name: please crate a valid csv file in the root of this directory");
-	// if (_csv_db.peek() == std::ifstream::traits_type::eof())
-	// 	throw std::invalid_argument("Empty file: the csv file is empty");
 }
 
 bitCoin::~bitCoin()
@@ -33,8 +31,6 @@ void	bitCoin::loadFiles(const std::string& argv){
 	if (!_value_db.is_open())
 		throw std::invalid_argument("Invalid file name: please pass as argument a valid \
 			.txt file which is in the root of this directory");
-	// if (_value_db.peek() == std::ifstream::traits_type::eof()) 
-	// 	throw std::invalid_argument("Empty file: the value file is empty");
 }
 
 void bitCoin::parse_csv(){
@@ -64,17 +60,15 @@ std::pair<std::string, t_date>	bitCoin::check_dates(const std::string& str){
 	
 	tmp_pair.second.flag = FALSE;
 	if (str.length() != 10 || isValidstr(str))
-		{
-			tmp_pair.second.error = "1Error: bad input => " + str;
+	{
+			tmp_pair.second.error = "Error: bad input => " + str;
 			tmp_pair.second.flag = TRUE;
-			//throw std::invalid_argument("Invalid date format: yyyy-mm-dd NOOOOO");
-		}
+	}
 	if (str[4] != '-' || str[7] != '-' || checkForHyphen(str) != 2)
-		{
-			tmp_pair.second.error = "2Error: bad input => " + str;
+	{
+			tmp_pair.second.error = "Error: bad input => " + str;
 			tmp_pair.second.flag = TRUE;
-			//throw std::invalid_argument("Invalid date format: yyyy-mm-dd NOOOOO");
-		}
+	}
 	temp = str.substr(0, 4);
 	tmp_pair.second.year = atoi(temp.c_str());
 	temp = str.substr(5, 2);
@@ -83,17 +77,15 @@ std::pair<std::string, t_date>	bitCoin::check_dates(const std::string& str){
 	tmp_pair.second.day = atoi(temp.c_str());
 	if ((tmp_pair.second.year < 2009 || tmp_pair.second.year > 2024) || (tmp_pair.second.month < 1 
 	|| tmp_pair.second.month > 12) || (tmp_pair.second.day < 1 || tmp_pair.second.day > calendar[tmp_pair.second.month]))
-		{
-			tmp_pair.second.error = "3Error: bad input => " + str;
+	{
+			tmp_pair.second.error = "Error: bad input => " + str;
 			tmp_pair.second.flag = TRUE;
-			//throw std::invalid_argument("Invalid date format: yyyy-mm-dd NOOOOO");
-		}
+	}
 	if(!isLeapYear(tmp_pair.second.year) && tmp_pair.second.month == 2 && tmp_pair.second.day > 28)
-		{
-			tmp_pair.second.error = "4Error: bad input => " + str;
+	{
+			tmp_pair.second.error = "Error: bad input => " + str;
 			tmp_pair.second.flag = TRUE;
-			//throw std::invalid_argument("Invalid date format: yyyy-mm-dd leap");
-		}
+	}
 	tmp_pair.first = str;
 	return(tmp_pair);
 }
@@ -156,8 +148,6 @@ void	bitCoin::printResult() {
 			tmp_pair = check_values(line.c_str() + (pos + 3), tmp_pair);
 
 		std::string tmp = tmp_pair.first;
-		// tmp.erase(tmp.size() - 2);
-		//tmp.append("11");
 		it = _csv_map.find(tmp);
 		if (tmp_pair.second.flag)
 			std::cout <<  tmp_pair.second.error << std::endl;
@@ -176,16 +166,13 @@ void bitCoin::find_closest(std::pair<std::string, t_date>&	tmp_pair) const{
 	int	day = tmp_pair.second.day;
 	int	month =  tmp_pair.second.month;
 	int	year =  tmp_pair.second.year;
-	std::cout <<  day << month << year << std::endl;
-
+	
 	std::map<std::string, t_date>::const_reverse_iterator rit;
 	for (rit=_csv_map.rbegin(); rit != _csv_map.rend(); ++rit)
 	{
 		if((rit->second.year <= year && rit->second.month <= month && rit->second.day <= day) \
 			|| (rit->second.year <= year && rit->second.month < month) || (rit->second.year < year))
 		{
-			std::cout << rit->first << " " 
-			<< rit->second.value << " " << tmp_pair.second.value << std::endl;
 			std::cout << rit->first << " => " 
 			<< rit->second.value * tmp_pair.second.value << std::endl;
 			break;
--- Module09/ex00/BitcoinExchangeUtils.cpp
+++ Module09/ex00/BitcoinExchangeUtils.cpp
@@ -6,7 +6,7 @@
 /*   By: juan-anm < juan-anm@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:20:04 by juan-anm          #+#    #+#             */
-/*   Updated: 2024/04/18 17:35:48 by juan-anm         ###   ########.fr       */
+/*   Updated: 2024/04/18 23:15:36 by juan-anm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ std::pair<std::string, t_date>& bitCoin::check_values(const std::string& value_s
 	tmp_pair.second.value = strtod(value_str.c_str(), NULL);
 	if (tmp_pair.second.value < 0)
 	{
-		tmp_pair.second.error = "Error: not a positive number : > " + value_str;
+		tmp_pair.second.error = "Error: not a positive number => " + value_str;
 		tmp_pair.second.flag = TRUE;
 	}
 	else if (tmp_pair.second.value > 1000)
 	{
-		tmp_pair.second.error = "Error: too large a number : > " + value_str;
+		tmp_pair.second.error = "Error: too large a number => " + value_str;
 		tmp_pair.second.flag = TRUE;
 	}
 	return tmp_pair;
